797a07d1d631a5bf3a76fefe5a73499c631e7610
jsgarzi <jsgarzi@fortitudedevelopment.com>
2020-10-30 09:07:30 -0700

    Julian- linked map and weather

--- script.js
+++ script.js
@@ -1,72 +1,71 @@
-// //MAP STUFF
-// let map;
+//MAP STUFF
+let map;
 
-// var currentLocation;
-// var options =
+var currentLocation;
 
 
-//     function initMap() {
-//         map = new google.maps.Map(document.getElementById("map"), {
-//             center: { lat: 40.7306, lng: -73.9352 },
-//             zoom: 12,
-//         });
-//         const input = document.getElementById("travel-input");
-//         const searchBox = new google.maps.places.SearchBox(input);
-//         map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(input);
-//         // Bias the SearchBox results towards current map's viewport.
-//         map.addListener("bounds_changed", () => {
-//             searchBox.setBounds(map.getBounds());
-//         });
-//         let markers = [];
-//         // Listen for the event fired when the user selects a prediction and retrieve
-//         // more details for that place.
-//         searchBox.addListener("places_changed", () => {
-//             const places = searchBox.getPlaces();
-//             console.log(places) //where the name is captured 
-//             currentLocation = places[0].name; //storing the name
-//             console.log(currentLocation);
-//             if (places.length == 0) {
-//                 return;
-//             }
-//             // Clear out the old markers.
-//             markers.forEach((marker) => {
-//                 marker.setMap(null);
-//             });
-//             markers = [];
-//             // For each place, get the icon, name and location.
-//             const bounds = new google.maps.LatLngBounds();
-//             places.forEach((place) => {
-//                 if (!place.geometry) {
-//                     console.log("Returned place contains no geometry");
-//                     return;
-//                 }
-//                 const icon = {
-//                     url: place.icon,
-//                     size: new google.maps.Size(71, 71),
-//                     origin: new google.maps.Point(0, 0),
-//                     anchor: new google.maps.Point(17, 34),
-//                     scaledSize: new google.maps.Size(25, 25),
-//                 };
-//                 // Create a marker for each place.
-//                 markers.push(
-//                     new google.maps.Marker({
-//                         map,
-//                         icon,
-//                         title: place.name,
-//                         position: place.geometry.location,
-//                     })
-//                 );
-//                 if (place.geometry.viewport) {
-//                     // Only geocodes have viewport.
-//                     bounds.union(place.geometry.viewport);
-//                 } else {
-//                     bounds.extend(place.geometry.location);
-//                 }
-//             });
-//             map.setOptions({ minZoom: 12, maxZoom: 16 }) //fixed zoom to make more sense on search of city
-//             map.fitBounds(bounds);
-//         });
-//     }
+    function initMap() {
+        map = new google.maps.Map(document.getElementById("map"), {
+            center: { lat: 40.7306, lng: -73.9352 },
+            zoom: 12,
+        });
+        const input = document.getElementById("travel-input");
+        const searchBox = new google.maps.places.SearchBox(input);
+        map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(input);
+        // Bias the SearchBox results towards current map's viewport.
+        map.addListener("bounds_changed", () => {
+            searchBox.setBounds(map.getBounds());
+        });
+        let markers = [];
+        // Listen for the event fired when the user selects a prediction and retrieve
+        // more details for that place.
+        searchBox.addListener("places_changed", () => {
+            const places = searchBox.getPlaces();
+            console.log(places) //where the name is captured 
+            currentLocation = places[0].name; //storing the name
+            console.log(currentLocation);
+            if (places.length == 0) {
+                return;
+            }
+            // Clear out the old markers.
+            markers.forEach((marker) => {
+                marker.setMap(null);
+            });
+            markers = [];
+            // For each place, get the icon, name and location.
+            const bounds = new google.maps.LatLngBounds();
+            places.forEach((place) => {
+                if (!place.geometry) {
+                    console.log("Returned place contains no geometry");
+                    return;
+                }
+                const icon = {
+                    url: place.icon,
+                    size: new google.maps.Size(71, 71),
+                    origin: new google.maps.Point(0, 0),
+                    anchor: new google.maps.Point(17, 34),
+                    scaledSize: new google.maps.Size(25, 25),
+                };
+                // Create a marker for each place.
+                markers.push(
+                    new google.maps.Marker({
+                        map,
+                        icon,
+                        title: place.name,
+                        position: place.geometry.location,
+                    })
+                );
+                if (place.geometry.viewport) {
+                    // Only geocodes have viewport.
+                    bounds.union(place.geometry.viewport);
+                } else {
+                    bounds.extend(place.geometry.location);
+                }
+            });
+            map.setOptions({ minZoom: 12, maxZoom: 16 }) //fixed zoom to make more sense on search of city
+            map.fitBounds(bounds);
+        });
+    }
 // /////////////////
 
 
