daa4f3c407d5a115d68043c4a06afdba2bd0b66c
jsgarzi <jsgarzi@fortitudedevelopment.com>
2020-10-30 09:15:24 -0700

    map & weather combined

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let map;
 
 var currentLocation;
 
+
     function initMap() {
         map = new google.maps.Map(document.getElementById("map"), {
             center: { lat: 40.7306, lng: -73.9352 },
@@ -63,59 +64,90 @@ var currentLocation;
             });
             map.setOptions({ minZoom: 12, maxZoom: 16 }) //fixed zoom to make more sense on search of city
             map.fitBounds(bounds);
-        
-            //function to get weather
-
         });
     }
-/////////////////
+// /////////////////
 
+
+
+
+//////////////// EVENT Test
+// var getEvents = [];
+// $.ajax({
+//   type: "GET",
+//   url: "https://app.ticketmaster.com/discovery/v2/events.json?size=4&apikey=ElWPP9FatyxVq4ke0f4mPT8u3LtGG04m",
+//   async: true,
+//   dataType: "json",
+//   success: function(json) {
+//       getEvents.json = json;
+//       showEvents(json);
+//       console.log(json);
+//       }
+// });
+// function showEvents(json) {
+//   var items = $('#events .list-group-item'); //// might want to try grabbing just the class and use the class at each index instead of using an id 
+//   var events = json._embedded.events;
+//   var item = items.first(); ////may not need
+//   for (var i=0; i< events.length; i++) {
+//     item.children('.list-group-item-heading').text(events[i].name);
+//     item.children('.list-group-item-text').text(events[i].dates.start.localDate);
+//     item.children('.list-group-url').text(events[i].url);
+//     try {
+//       item.children('.venue').text(events[i]._embedded.venues[0].name + " in " + events[i]._embedded.venues[0].city.name + " url " + events[i]._embedded.venues[0].url);
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   }
+// }
 
 // Weather API
-$(document).ready(function () {
 
+$(document).ready(function () {
 
     var APIkey = "11aae01829609ac12c0335ac0cc4505c";
 
-    $(".travel-input").bind("enterKey", function (e) {
+    $("#travel-input").keypress(function (e) {
+        if (e.which == 13) {
 
-        var userInput = $("#search-box").val();
+            var userInput = $("#travel-input").val();
 
-        var currURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + APIkey + "&units=imperial"
+            var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + APIkey + "&units=imperial"
 
-        /// sucessfully pulling the lat and long at the desired area 
-        $.ajax({
-            url: currURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
 
-            var weathResults = response;
-            console.log(weathResults);
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+                console.log(response);
 
-            var lat = response.coord.lat;
-            var lon = response.coord.lon;
-            var temp = response.main.temp;
-            var weather = response.weather[0].icon;
+                var weathResults = response;
+                console.log(weathResults);
+                
+                var city = response.name;
+                var lat = response.coord.lat;
+                var lon = response.coord.lon;
+                var temp = response.main.temp;
+                var weather = response.weather[0].icon;
 
-            console.log(lat, lon);
+                console.log(lat, lon);
 
-            var curDiv = $("<div>");
-            var pTemp = $("<p>").text("Temperature: " + temp + "F");
-            // var pWeath = $("<p>").text("Weather: " + weather);
-            var weathIcon = $("<img>").attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
-            curDiv.append(pTemp, weathIcon)
+                var curDiv = $("<div>");
+                var pCity = $("<p>").text(city);
+                var pTemp = $("<p>").text("Temperature: " + temp + "F");
+                // var pWeath = $("<p>").text("Weather: " + weather);
+                var weathIcon = $("<img>").attr("src", "http://openweathermap.org/img/w/" + weather + ".png");
+                curDiv.append(pCity, pTemp, weathIcon)
 
-            $("#current").append(curDiv)
+                $("#current").append(curDiv)
 
-            sevenDay(lat, lon)
+                sevenDay(lat, lon)
 
-        })
+            })
+        }
     });
-    // need to work out way to tie the data that is being successfully pulled to the prebuilt cards
     function sevenDay(lat, lon) {
 
-        var userInput = $("#search-box").val();
+        var userInput = $("#travel-input").val();
 
         var sevDURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={current}" + "&appid=" + APIkey + "&units=imperial"
 
@@ -133,75 +165,40 @@ $(document).ready(function () {
             var daily = response.daily
 
 
-            for (var i = 0; i < daily.length; i++) { 
-                //is this giving back 8 days?
-                var unixTime = daily[i].dt
-                var date = moment.unix(unixTime).format("MM, DD, YYYY")
-                console.log(date);
+            // for (var i = 0; i < daily.length; i++) {
+            //     var unixTime = daily[i].dt
+            //     var date = moment.unix(unixTime).format("MM, DD, YYYY")
+            //     console.log(date);
+
+            // }
+            // moment(daily[0].dt_txt).format('MMMM Do YYYY')
+            $(".card-content").empty();
+
+            for (var i = 0; i < daily.length; i++) {
+
+            var dayoneDiv = $("<div>")
+
+
+            // var date = $("<p>").text(moment(daily[0].dt_txt).format('MMMM Do YYYY'));
+            var pTemp = $("<p>").text("Temperature " + response.daily[i].temp.day + "F");
+            console.log(pTemp)
+            var pWeath = $("<img>").attr("src", "http://openweathermap.org/img/w/" + daily[i].weather[0].icon + ".png")
+            console.log(pWeath)
+            dayoneDiv.append(pTemp, pWeath)
+            $("#card"+[i]).append(dayoneDiv)
+
+            console.log(daily[i].temp.day)
+
 
             }
-            // moment(daily[0].dt_txt).format('MMMM Do YYYY')
-                $("#seven-days").empty();
 
-                for (var i = 0; i < daily.length; i++) {
-
-                    var dayoneDiv = $("<div>")
-                    
-
-                    // var date = $("<p>").text(moment(daily[0].dt_txt).format('MMMM Do YYYY'));
-                    var pTemp = $("<p>").text(response.daily[i].temp.day + "F");
-                    var pWeath = $("<img>").attr("src", "http://openweathermap.org/img/w/" + daily[i].weather[0].icon + ".png")
-                    dayoneDiv.append(pTemp, pWeath)
-                    $("#seven-days").prepend(dayoneDiv)
-
-                    console.log(daily[i].temp.day)
-
-                }
-            
 
 
         })
 
 
     };
-    
-    $('textarea').keyup(function(e){
-        if(e.keyCode == 13)
-        {
-            $(this).trigger("enterKey");
-        }
-    });
+
+
+
 });
-
-
-
-
-
-///events 
-var getEvents = [];
-$.ajax({
-  type: "GET",
-  url: "https://app.ticketmaster.com/discovery/v2/events.json?size=4&apikey=ElWPP9FatyxVq4ke0f4mPT8u3LtGG04m",
-  async: true,
-  dataType: "json",
-  success: function(json) {
-      getEvents.json = json;
-      showEvents(json);
-      console.log(json);
-      }
-});
-function showEvents(json) {
-  var items = $('#events .list-group-item'); //change the tags being picked here?
-  var events = json._embedded.events;
-  var item = items.first(); ///frist? is this 
-  for (var i=0; i< events.length; i++) {
-    item.children('.list-group-item-heading').text(events[i].name);
-    item.children('.list-group-item-text').text(events[i].dates.start.localDate);
-    item.children('.list-group-url').text(events[i].url);
-    try {
-      item.children('.venue').text(events[i]._embedded.venues[0].name + " in " + events[i]._embedded.venues[0].city.name + " url " + events[i]._embedded.venues[0].url);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-}
