24f9951b2b746a93309c653cc2bf8f3ac7d8b88e
Neysalee Acosta <nacosta@clickpay.com>
2020-10-31 09:57:08 -0400

    Fixed travel-input selection for TM

--- script.js
+++ script.js
@@ -4,121 +4,115 @@ let map;
 var currentLocation;
 
 
-    function initMap() {
-        map = new google.maps.Map(document.getElementById("map"), {
-            center: { lat: 40.7306, lng: -73.9352 },
-            zoom: 12,
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 40.7306, lng: -73.9352 },
+        zoom: 12,
+    });
+    const input = document.getElementById("travel-input");
+    const searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(input);
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener("bounds_changed", () => {
+        searchBox.setBounds(map.getBounds());
+    });
+    let markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener("places_changed", () => {
+        const places = searchBox.getPlaces();
+        console.log(places) //where the name is captured 
+        currentLocation = places[0].name; //storing the name
+        console.log(currentLocation);
+        city.push(currentLocation);
+        if (places.length == 0) {
+            return;
+        }
+        // Clear out the old markers.
+        markers.forEach((marker) => {
+            marker.setMap(null);
         });
-        const input = document.getElementById("travel-input");
-        const searchBox = new google.maps.places.SearchBox(input);
-        map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(input);
-        // Bias the SearchBox results towards current map's viewport.
-        map.addListener("bounds_changed", () => {
-            searchBox.setBounds(map.getBounds());
-        });
-        let markers = [];
-        // Listen for the event fired when the user selects a prediction and retrieve
-        // more details for that place.
-        searchBox.addListener("places_changed", () => {
-            const places = searchBox.getPlaces();
-            console.log(places) //where the name is captured 
-            currentLocation = places[0].name; //storing the name
-            console.log(currentLocation);
-            city.push(places);
-            if (places.length == 0) {
+        markers = [];
+        // For each place, get the icon, name and location.
+        const bounds = new google.maps.LatLngBounds();
+        places.forEach((place) => {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
                 return;
             }
-            // Clear out the old markers.
-            markers.forEach((marker) => {
-                marker.setMap(null);
-            });
-            markers = [];
-            // For each place, get the icon, name and location.
-            const bounds = new google.maps.LatLngBounds();
-            places.forEach((place) => {
-                if (!place.geometry) {
-                    console.log("Returned place contains no geometry");
-                    return;
-                }
-                const icon = {
-                    url: place.icon,
-                    size: new google.maps.Size(71, 71),
-                    origin: new google.maps.Point(0, 0),
-                    anchor: new google.maps.Point(17, 34),
-                    scaledSize: new google.maps.Size(25, 25),
-                };
-                // Create a marker for each place.
-                markers.push(
-                    new google.maps.Marker({
-                        map,
-                        icon,
-                        title: place.name,
-                        position: place.geometry.location,
-                    })
-                );
-                if (place.geometry.viewport) {
-                    // Only geocodes have viewport.
-                    bounds.union(place.geometry.viewport);
-                } else {
-                    bounds.extend(place.geometry.location);
-                }
-            });
-            map.setOptions({ minZoom: 12, maxZoom: 16 }) //fixed zoom to make more sense on search of city
-            map.fitBounds(bounds);
+            const icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25),
+            };
+            // Create a marker for each place.
+            markers.push(
+                new google.maps.Marker({
+                    map,
+                    icon,
+                    title: place.name,
+                    position: place.geometry.location,
+                })
+            );
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
         });
-    }
+        map.setOptions({ minZoom: 12, maxZoom: 16 }) //fixed zoom to make more sense on search of city
+        map.fitBounds(bounds);
+    });
+}
 ///////////////
 
 var city = [];
 console.log(city);
 
-$("#travel-input").click(function() {
+$("#travel-input").keypress(function (e) {
+    if (e.which === 13) {
 
-	$(this).attr("city");
+        var searchedCity = $("#travel-input").val()
 
-
-	// function getLocation() {
-	//     if (navigator.geolocation) {
-	//         navigator.geolocation.getCurrentPosition(showPosition);
-	// }
-	// }
-
-	// function showPosition(position) {
-		// var latlon = position.coords.latitude + "," + position.coords.longitude;
-
-		var getEvents = [];
+        $(this).attr("city.name");
 
 
-		$.ajax({
-			type: "GET",
-			url: "https://app.ticketmaster.com/discovery/v2/events.json?size=4&apikey=ElWPP9FatyxVq4ke0f4mPT8u3LtGG04m&city=" + city,
-			async: true,
-			dataType: "json",
-			success: function(json) {
-				getEvents.json = json;
-				showEvents(json);
-				console.log(json);
-			},
-			error: function(xhr, status, err) {
-				console.log(err);
-			}
-		});
-
-    function showEvents(json) {
-        var events = json._embedded.events;
-        for (var i = 0; i < events.length; i++) {
-            var newDiv = $("<div>");
-            var pName = $("<p>").text(json._embedded.events[i].name);
-            var pDate = $("<p>").text(json._embedded.events[i].dates.start.localDate);
-            var pLink = $("<p>").text(json._embedded.events[i].url);
-            newDiv.append(pName, pDate, pLink);
-
-            $("#card-content-event" + [i]).append(newDiv);
-        }
-    };
+        var getEvents = [];
 
 
+        $.ajax({
+            type: "GET",
+            url: "https://app.ticketmaster.com/discovery/v2/events.json?size=4&apikey=ElWPP9FatyxVq4ke0f4mPT8u3LtGG04m&city=" + searchedCity,
+            async: true,
+            dataType: "json",
+            success: function (json) {
+                getEvents.json = json;
+                showEvents(json);
+                console.log(json);
+            },
+            error: function (xhr, status, err) {
+                console.log(err);
+            }
+        });
 
-// getLocation();
+        function showEvents(json) {
+            console.log(json);
+            var events = json._embedded.events;
+            for (var i = 0; i < events.length; i++) {
+                var newDiv = $("<div>");
+                var pName = $("<p>").text(json._embedded.events[i].name);
+                var pDate = $("<p>").text(json._embedded.events[i].dates.start.localDate);
+                var pLink = $("<p>").text(json._embedded.events[i].url);
+                newDiv.append(pName, pDate, pLink);
+
+                $("#card-content-event" + [i]).append(newDiv);
+            }
+        };
+
+
+    }
 
 });
